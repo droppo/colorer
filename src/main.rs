5b76eae6cc0c77ab31b3f2ea9ad7adc12f3e5161
droppo <paolo.afrune@gmail.com>
2021-09-01 16:24:18 +0200

    refactoring main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{
     convert::TryInto,
     env,
     io::{BufRead, BufReader},
-    process::{Command, Stdio},
+    process::{self, Command, Stdio},
     thread,
 };
 
@@ -18,27 +18,48 @@ mod parser;
 fn run_colorer() {
     let args: Vec<String> = env::args().collect();
     if let Some(command) = args.get(1) {
-        if let Some(p) = parser::parser::init_parser(&command) {
+        if let Some(p) = parser::parser::init_parser(command) {
             let mut arguments = vec![];
             args.iter().skip(2).for_each(|arg| arguments.push(arg));
 
-            let child = Command::new(command)
+            let child = match Command::new(command)
                 .args(&arguments)
                 .stdout(Stdio::piped())
                 .stderr(Stdio::piped())
                 .spawn()
-                // commands are filtered during init phase,
-                // unwrap is considered secure here
-                .unwrap();
+            {
+                Ok(c) => c,
+                Err(_) => {
+                    eprintln!("clrr: command not found: {}", args.get(1).unwrap());
+                    process::exit(1);
+                }
+            };
 
             let child_pid = child.id();
-            ctrlc::set_handler(move || {
+            let res = ctrlc::set_handler(move || {
                 kill(Pid::from_raw(child_pid.try_into().unwrap()), SIGINT).unwrap();
-            })
-            .unwrap(); // TODO manage unwrap
+            });
 
-            let stdout_child = child.stdout.unwrap(); // TODO
-            let stderr_child = child.stderr.unwrap(); // TODO
+            if res.is_err() {
+                eprintln!("clrr: failed to set handler");
+                process::exit(1);
+            }
+
+            let stdout_child = match child.stdout {
+                Some(stdout) => stdout,
+                None => {
+                    eprintln!("clrr: failed to hook stdout");
+                    process::exit(1)
+                }
+            };
+
+            let stderr_child = match child.stderr {
+                Some(stderr) => stderr,
+                None => {
+                    eprintln!("clrr: failed to hook stderr");
+                    process::exit(1);
+                }
+            };
 
             let p_out = p.clone();
             let p_err = p.clone();
@@ -61,10 +82,12 @@ fn run_colorer() {
                     });
             });
 
-            stderr_th.join().unwrap(); // TODO
-            stdout_th.join().unwrap(); // TODO
+            if stderr_th.join().is_err() || stdout_th.join().is_err() {
+                eprintln!("clrr: can't join thread");
+                process::exit(1);
+            }
         } else {
-            eprintln!("command {} not supported", command);
+            eprintln!("clrr: command {} not supported", command);
         }
     } else {
         eprintln!("failed to start colorer: at least one argument is required");
